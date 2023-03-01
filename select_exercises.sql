e2c2f0043693290c2f4f75e06017331bb830ffa3
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-01 14:47:28 -0600

    update complete

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,24 +1,24 @@
 USE ymir_stephen;
 -- The name of all albums by Pink Floyd.
--- SELECT 'displaying all albums by pink floyd' AS 'Getting Albums';
-SELECT artist, name_record FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'Album: ' AS 'Pink Floyd''s Albums',
+name_record FROM albums WHERE artist = 'Pink Floyd';
 
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
--- SELECT 'displaying year SPLHCB was released' AS 'Getting SGT Pepper''s release date';
-SELECT name_record, release_date FROM albums WHERE name_record = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'Sgt. Pepper''s Lonely Hearts Club Band' AS 'Album',
+release_date FROM albums WHERE name_record = 'Sgt. Pepper''s Lonely Hearts Club Band' AS 'release date';
 
 -- The genre for Nevermind (this does not exist)
--- SELECT 'displaying genre for Nevermind' AS 'Nirvana Nevermind Genre';
-SELECT artist, genre FROM albums WHERE name_record = 'Nevermind';
+SELECT 'Genre for SPLHCB: ' AS 'SPLHCB',
+genre FROM albums WHERE name_record = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 -- Which albums were released in the 1990s
--- SELECT 'displaying albums released in the 90''s' AS 'ALL album''s released in the 90''s';
-SELECT artist, name_record FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT 'Artist | Record' AS '90''s album''s',
+artist, name_record FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
 -- Which albums had less than 20 million certified sales
--- SELECT 'displaying albums with less than 20 million certified sales' AS 'Worst of the best artist sales';
-SELECT artist, name_record, sales FROM albums WHERE sales < 20;
+SELECT 'Artist | Album | Sales(millions)' AS 'Worst of the best artist sales',
+artist, name_record, sales FROM albums WHERE sales < 20;
 
 -- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
--- SELECT 'displaying Rock albums' AS 'Rock Albums';
-SELECT artist, name_record, genre FROM albums WHERE genre = 'Rock';
+SELECT 'Artist | Album' AS 'Rock Albums',
+artist, name_record FROM albums WHERE genre = 'Rock';

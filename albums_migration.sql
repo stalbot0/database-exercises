8ff23b64e85bb81f39379f391e9dac34dd3ddb20
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-01 12:11:35 -0600

    working basic statement exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,14 +2,14 @@ USE ymir_stephen;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    release_date DATE NOT NULL,
-    sales FLOAT(9, 2) NOT NULL,
-    genre VARCHAR(250) NOT NULL,
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist CHAR(150) NOT NULL,
+    name_record CHAR(150) NOT NULL,
+    release_date INT(4) NOT NULL,
+    sales DECIMAL(9, 2) NOT NULL,
+    genre CHAR(250) NOT NULL,
     PRIMARY KEY (id)
-);
+    );
 
 SHOW CREATE TABLE albums;
 
---
+++ albums_seeder.sql
@@ -0,0 +1,25 @@
+USE ymir_stephen;
+INSERT INTO albums(artist, name_record, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 50.20, 'Pop, post-disco, funk, rock'),
+       ('AC/DC', 'Back in Black', 1980, 30.10, 'Hard rock'),
+       ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.80, 'Progressive rock'),
+       ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.20, 'Country rock, soft rock, folk rock'),
+       ('Eagles', 'Hotel California', 1976, 31.80, 'Soft rock'),
+       ('Fleetwood Mac', 'Rumours', 1977, 29.30, 'Soft rock'),
+       ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.40, 'Hard rock, heavy metal, folk roc'),
+       ('Michael Jackson', 'Bad', 1987, 21.10, 'Pop, rhythm and blues, funk and '),
+       ('Meat Loaf', 'Bat Out of Hell', 1977, 22.00, 'Hard rock, glam rock, progressive rock'),
+       ('Various artists', 'Dirty Dancing', 1987, 24.10, 'Pop, rock, R&B'),
+       ('Metallica', 'Metallica', 1991, 22.70, 'Heavy metal'),
+       ('Guns N'' Roses', 'Appetite for Destruction', 1987, 22.80, 'Hard rock'),
+       ('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers', 1984, 22.90, 'Reggae'),
+       ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 22.00, 'Heartland rock'),
+       ('Dire Straits', 'Brothers in Arms', 1985, 21.10, 'Roots rock, blues rock, soft rock'),
+       ('Madonna', 'The Immaculate Collection ', 1990, 19.80, 'Pop, dance'),
+       ('Pink Floyd', 'The Wall', 1979, 18.90, 'Progressive rock'),
+       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.30, 'Rock'),
+       ('Michael Jackson', 'Dangerous', 1991, 17.00, 'New jack swing, R&B and pop'),
+       ('The Beatles', 'Abbey Road', 1969, 16.80, 'Rock');
+--        ('Michael Jackson', 'Thriller2', 1900, 2.00, 'stuff');
+--          this is giving me problems with the year, why?
+

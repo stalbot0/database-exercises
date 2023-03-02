8f16d88c4506616435e6a0fc925200d25d332f78
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-02 13:50:34 -0600

    completed functions_exercises for SQL

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,13 +15,13 @@ ORDER BY emp_no DESC;
 SELECT first_name, last_name, birth_date
 FROM employees
 WHERE (MONTH(birth_date) = 12
-  AND DAY(birth_date) = 25)
+    AND DAY(birth_date) = 25)
 ORDER BY YEAR(birth_date) DESC;
 
 # All employees hired in the 90's AND born on xmas, how many days they have been working at the company
-SELECT CONCAT(first_name, ' ', last_name), birth_date, hire_date, DATEDIFF(DAY(hire_date), DAY(NOW()))
+SELECT CONCAT(first_name, ' ', last_name), birth_date, hire_date, CONCAT(DATEDIFF(CURDATE(), hire_date), ' days since hire')
 FROM employees
 WHERE (YEAR(hire_date) BETWEEN 1990 AND 1999
     AND MONTH(birth_date) = 12
     AND DAY(birth_date) = 25)
-ORDER BY YEAR(birth_date) ASC, YEAR(hire_date) DESC;
+ORDER BY YEAR(birth_date), YEAR(hire_date) DESC;

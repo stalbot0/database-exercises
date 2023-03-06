c513edbb1495ef500efc9536341493919871040a
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-06 14:20:07 -0600

    updated syntax

--- adlister.sql
+++ adlister.sql
@@ -2,11 +2,11 @@ USE ymir_stephen;
 # Write SQL queries to answer the following questions:
 #
 # For a given ad, what is the email address of the user that created it?
-SELECT u.email 'Email Address'
+SELECT u.email 'Email Address for Advertisement'
 FROM users u
     JOIN ads a
         ON u.id = a.user_id
-WHERE a.title = '2005 Mustang GT';
+WHERE a.title = 'Forks';
 
 # For a given ad, what category, or categories, does it belong to?
 SELECT a.title 'Ad Title',c.category 'Ad Categories'
@@ -15,7 +15,7 @@ FROM categories c
         ON c.id = ca.category_id
     JOIN ads a
         ON ca.ad_id = a.id
-WHERE a.title = '2005 Mustang GT';
+WHERE a.title = '2005 Mustang GT' OR a.title = '1986 Chrysler K Car';
 
 # For a given category, show all the ads that are in that category.
 SELECT a.title 'Ads in Cars Category'
@@ -24,7 +24,8 @@ FROM ads a
         ON a.id = ca.ad_id
     JOIN categories c
         ON ca.category_id = c.id
-WHERE category = 'Cars';
+WHERE c.category = 'Cars' OR c.category = 'Automotive'
+group by a.title;
 
 #     For a given user, show all the ads they have posted.
 SELECT u.email "User's email", a.title "Ads they've posted"
